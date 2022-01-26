ea764b04a007d3d039f95d9fd033529dc0d77772
Lolpawer <dadu.petrosino@gmail.com>
2022-01-26 16:40:11 +0100

    bonus jumbotron added

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <jumbotron>
+        JUMBO
+    </jumbotron>
+</template>
+
+<script>
+</script>
+
+<style>
+jumbotron {
+    background-color: darkblue;
+    color: darkred;
+    font-size: 500px;
+    padding: 20px 10px;
+}
+</style>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,3 +1,9 @@
 <template>
     <h1>Titolo della pagina! Incredibile!</h1>
 </template>
+
+<script>
+</script>
+
+<style>
+</style>
